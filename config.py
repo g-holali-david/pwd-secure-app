d3f270244d12722ec93a2c42600bdd8b8ea05ae2
GAVI Holali David <167685916+g-holali-david@users.noreply.github.com>
2025-02-27 15:39:15 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_USERNAME = os.getenv("REDIS_USERNAME", None)
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+    PEPPER = os.getenv("PEPPER", "PoivreSecret123").encode('utf-8')
