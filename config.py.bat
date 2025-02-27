245ff05e9a45eddeafb9b9b38a0240312a23308c
Davs-Holali <davs.itcom@gmail.com>
2025-02-27 15:28:28 +0100

    ok

---
+++ config.py.bat
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = "super_secret_key"
+    REDIS_HOST = "redis-17750.c339.eu-west-3-1.ec2.redns.redis-cloud.com"
+    REDIS_PORT = 17750
+    REDIS_USERNAME = "default"
+    REDIS_PASSWORD = "JpJ7bzNe7ii9KlwIn6QOPBZWwBafkPhR"
+    PEPPER = b"PoivreSecret123"
