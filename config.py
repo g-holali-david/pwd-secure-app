caad3561d5f0fe79a29bc5b01b60a067e5f3ebe6
Davs-Holali <davs.itcom@gmail.com>
2025-02-27 15:56:19 +0100

    Fix: Load environment variables

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
     REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
