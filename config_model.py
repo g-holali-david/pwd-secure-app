f2ce1efee620ed5506b533d4ad2aa296f6a1ccf9
GAVI Holali David <167685916+g-holali-david@users.noreply.github.com>
2025-02-27 15:03:46 +0100

    Delete config_model.py

--- config_model.py
+++
@@ -1,8 +0,0 @@
-import os
-class Config:
-    SECRET_KEY = "super_secret_key"
-    REDIS_HOST = "redis-......."
-    REDIS_PORT = 17750
-    REDIS_USERNAME = "default"
-    REDIS_PASSWORD = ""
-    PEPPER = b"PoivreSecret123"
